51fdf67fdaa1471fc5b824d342996b282ef05084
Stian Strand <stianbekkelund@hotmail.com>
2020-05-26 11:26:18 +0200

    pf-ma-02_finished

---
+++ script.js
@@ -0,0 +1,64 @@
+// question 1
+
+// your answer for question 1 goes here
+let petArray = ["cat", "cow", "dog"];
+
+// question 2
+
+// your answer for question 2 goes here
+console.log(petArray[1]);
+
+// question 3
+
+// your answer for question 3 goes here
+petArray.push("sheep");
+
+// question 4
+
+// your answer for question 4 goes here
+console.log(petArray.length);
+
+// question 5
+
+// your answer for question 5 goes here
+var catObject = {
+  name: "whiskers",
+  colour: "black and green",
+  age: 15,
+};
+
+// question 6
+
+// your answer for question 6 goes here
+console.log(catObject.age);
+
+// question 7
+
+// your answer for question 7 goes here
+let catArray = [
+  {
+    name: "whiskers",
+    colour: "black and green",
+    age: 15,
+  },
+];
+
+// question 8
+
+// your answer for question 8 goes here
+for (var i = 0; i < catArray.length; i++) {
+  console.log(catArray[i]);
+}
+
+// question 9
+
+// your answer for question 9 goes here
+function logToConsole(isLogged) {
+  console.log("this is what i consolelog " + isLogged);
+}
+
+
+// question 10
+
+// your answer for question 10 goes here
+logToConsole("donkey");
